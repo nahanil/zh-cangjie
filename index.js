3d1fde1618b7decd35d06f0bc404442ec12737c3
Jarrod Linahan <jarrod@texh.net>
2015-06-28 21:11:54 +1000

    First commit. v0.0.1

---
+++ index.js
@@ -0,0 +1,52 @@
+var cangjie = {
+  A: "日",
+  B: "月",
+  C: "金",
+  D: "木",
+  E: "水",
+  F: "火",
+  G: "土",
+  H: "竹",
+  I: "戈",
+  J: "十",
+  K: "大",
+  L: "中",
+  M: "一",
+  N: "弓",
+  O: "人",
+  P: "心",
+  Q: "手",
+  R: "口",
+  S: "尸",
+  T: "廿",
+  U: "山",
+  V: "女",
+  W: "田",
+  X: "重",
+  Y: "卜",
+  Z: "Z"
+}
+
+function toCangjie(string) {
+  var out = [];
+  string.split("").forEach(function(l){
+    out.push( cangjie[l] || l );  
+  });
+  return out.join("");
+}
+
+function fromCangjie(string) {
+  var out = [];
+  string.split("").forEach(function(l){
+    for (key in cangjie) {
+      if (cangjie[key] == l) {
+        return out.push( key );  
+      }
+    }
+    out.push(l);
+  });
+  return out.join("");
+}
+
+module.exports.toCangjie   = toCangjie;
+module.exports.fromCangjie = fromCangjie;
---
+++ test/index.js
@@ -0,0 +1,40 @@
+var should  = require('chai').should(),
+    cangjie = require('../index');
+
+
+describe('#toCanjie', function() {
+  it('should convert MGILG to 一土戈中土', function() {
+    cangjie.toCangjie("MGILG").should.equal("一土戈中土");
+  });
+
+  it('should convert I to 戈', function() {
+    cangjie.toCangjie("I").should.equal("戈");
+  });
+
+  it('should convert ONF to 人弓火', function() {
+    cangjie.toCangjie("ONF").should.equal("人弓火");
+  });
+
+  it('should ignore unknown characters', function(){
+    cangjie.toCangjie("O!NF?").should.equal("人!弓火?");
+  });
+});
+
+describe('#fromCanjie', function() {
+  it('should convert 一土戈中土 to MGILG', function() {
+    cangjie.fromCangjie("一土戈中土").should.equal("MGILG");
+  });
+
+  it('should convert 戈 to I', function() {
+    cangjie.fromCangjie("戈").should.equal("I");
+  });
+
+  it('should convert 人弓火 to ONF', function() {
+    cangjie.fromCangjie("人弓火").should.equal("ONF");
+  });
+
+  it('should ignore unknown characters', function(){
+    cangjie.fromCangjie("人!弓火?").should.equal("O!NF?");
+  });
+});
+
