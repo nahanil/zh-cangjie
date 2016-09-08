8d82bbb8662a6b77335210860510b47ab9d957e9
Jarrod Linahan <jarrod@texh.net>
2016-09-09 08:35:51 +1000

    Add character lookup functionality

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./cangjie.db');
+
 var cangjie = {
   A: "日",
   B: "月",
@@ -27,7 +30,9 @@ var cangjie = {
   Z: "Z"
 }
 
-function toCangjie(string) {
+function ZHCangjie() {}
+
+ZHCangjie.prototype.toCangjie = function(string) {
   var out = [];
   if (!string) { return; }
   string.toUpperCase().split("").forEach(function(l){
@@ -36,7 +41,7 @@ function toCangjie(string) {
   return out.join("");
 }
 
-function fromCangjie(string) {
+ZHCangjie.prototype.fromCangjie = function(string) {
   var out = [];
   if (!string) { return; }
   string.split("").forEach(function(l){
@@ -50,5 +55,67 @@ function fromCangjie(string) {
   return out.join("");
 }
 
-module.exports.toCangjie   = toCangjie;
-module.exports.fromCangjie = fromCangjie;
+// Find the cangjie equivalent of a given character
+ZHCangjie.prototype.forCharacter = function(string, callback){
+  var $this = this;
+  var query = "SELECT * FROM cangjie WHERE character = ?";
+
+  db.get(query, string, function(err, chr) {
+    if (err) {
+      return callback(err);
+    }
+
+    if (!chr) {
+      return callback();
+    }
+
+    callback(null, chr.cangjie, $this.toCangjie(chr.cangjie));
+  });
+}
+
+// Find a list of characters that fully match given cangjie string
+ZHCangjie.prototype.__doLookupMulti = function(query, string, callback) {
+
+  if (string && !string.match(/^[A-Z]$/i)) {
+    string = this.fromCangjie(string);
+  }
+
+  db.all(query, string, function(err, found) {
+    if (err) {
+      return callback(err);
+    }
+
+    if (!found) {
+      return callback();
+    }
+
+    var chars = [];
+    found.forEach(function(f){
+      chars.push( f.character );  
+    });
+
+    callback(null, chars);
+  });
+
+}
+
+ZHCangjie.prototype.findCharacter = function(string, callback){
+  var $this = this;
+  var query = "SELECT * FROM cangjie WHERE cangjie = UPPER(?) "
+            + "ORDER BY frequency ASC";
+
+  this.__doLookupMulti(query, string, callback);
+}
+
+ZHCangjie.prototype.search = function(string, callback) {
+  var query = "SELECT * FROM cangjie WHERE cangjie LIKE UPPER(?) "
+            + "ORDER BY frequency ASC";
+            
+  if (string && string.substr(-1) != "%") {
+    string += "%";
+  }
+
+  this.__doLookupMulti(query, string, callback);
+}
+
+module.exports = new ZHCangjie();
